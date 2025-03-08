4e3332741100ffca0b84feb3fd2d7b9ef3baf31f
FICazk <1545028284@qq.com>
2025-03-08 23:03:11 +0800

    修改run.bat，防止重复下载

--- run.bat
+++ run.bat
@@ -1,3 +1,11 @@
+@echo off
+set "subdir=venv"
+
+if exist "%cd%\%subdir%\" (
+    start "" .\\venv\\Scripts\\pythonw .\\window.py
+    exit
+)
+
 python -m venv venv
 
 .\\venv\\Scripts\\pip3.exe install -r .\\requirements.txt
